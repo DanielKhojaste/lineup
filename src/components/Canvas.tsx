107d6d94472b0c418f0dfeb6c4785f7ffbb90a67
DanielKhojaste <danielkhojaste101@gmail.com>
2026-02-03 11:25:03 -0500

    feat: node now takes in the x and y coordinates as props

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,18 +1,26 @@
+import { useState, useEffect } from "react";
 import { DndContext } from "@dnd-kit/core";
 import Node from "./nodes/Node";
 
+type NodeType = {
+	id: string;
+	x: number;
+	y: number;
+};
+
 function Canvas() {
-	type Node = {
-		id: string;
-		x: number;
-		y: number;
-	};
+	const initialNodes: NodeType[] = [
+		{ id: "player1", x: 34, y: 56 },
+		{ id: "player2", x: 128, y: 80 },
+	];
+
+	const [nodes, setNodes] = useState<NodeType[]>([]);
 
 	return (
 		<div>
 			<DndContext>
 				<div className="canvas">
-					<Node id="box-1" />
+					<Node id="node1" x={0} y={0} />
 				</div>
 			</DndContext>
 		</div>
--- src/components/nodes/Node.tsx
+++ src/components/nodes/Node.tsx
@@ -1,10 +1,18 @@
 import { useDraggable } from "@dnd-kit/core";
 import { CSS } from "@dnd-kit/utilities";
 
-function Node({ id }: { id: string }) {
+type NodeProps = {
+	id: string;
+	x: number;
+	y: number;
+};
+
+function Node({ id, x, y }: NodeProps) {
 	const { attributes, listeners, setNodeRef, transform } = useDraggable({ id });
 
 	const style = {
+		left: x,
+		top: y,
 		transform: CSS.Translate.toString(transform),
 	};
 
