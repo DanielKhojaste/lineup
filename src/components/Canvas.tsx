dabcc484125b1ba36062bfdd95c862d4343eae5a
DanielKhojaste <danielkhojaste101@gmail.com>
2026-02-04 11:47:31 -0500

    feat: nodes are now fully draggable and will maintain their positions

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { DndContext } from "@dnd-kit/core";
+import { DndContext, DragEndEvent } from "@dnd-kit/core";
 import Node from "./nodes/Node";
 
 type NodeType = {
@@ -9,18 +9,31 @@ type NodeType = {
 };
 
 function Canvas() {
-	const initialNodes: NodeType[] = [
-		{ id: "player1", x: 34, y: 56 },
-		{ id: "player2", x: 128, y: 80 },
-	];
+	const [nodes, setNodes] = useState<NodeType[]>([
+		{ id: "player1", x: 0, y: 0 },
+		{ id: "player2", x: 34, y: 56 },
+		{ id: "player3", x: 128, y: 80 },
+	]);
 
-	const [nodes, setNodes] = useState<NodeType[]>([]);
+	function handleDragEnd(event: DragEndEvent) {
+		const { active, delta } = event;
+
+		setNodes((prev) =>
+			prev.map((node) =>
+				node.id === active.id
+					? { ...node, x: node.x + delta.x, y: node.y + delta.y }
+					: node,
+			),
+		);
+	}
 
 	return (
 		<div>
-			<DndContext>
+			<DndContext onDragEnd={handleDragEnd}>
 				<div className="canvas">
-					<Node id="node1" x={0} y={0} />
+					{nodes.map((node) => (
+						<Node id={node.id} x={node.x} y={node.y} key={node.id} />
+					))}
 				</div>
 			</DndContext>
 		</div>
