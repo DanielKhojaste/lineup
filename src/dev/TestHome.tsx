128e608a69e927695606907cbb1040272a3135ea
DanielKhojaste <danielkhojaste101@gmail.com>
2026-03-09 16:57:08 -0400

    chore(dev): add TestHome view for new DnD engine playground

---
+++ src/dev/TestHome.tsx
@@ -0,0 +1,9 @@
+function TestHome() {
+	return (
+		<main className="testHome view">
+			<h1>Test Home</h1>
+		</main>
+	);
+}
+
+export default TestHome;
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,18 +1,24 @@
 // src/router/routes.tsx
 import { lazy } from "react";
 import { RouteObject } from "react-router-dom";
+import TestHome from "../dev/TestHome";
 
 const Home = lazy(() => import("../views/Home"));
 const NotFound = lazy(() => import("../views/NotFound"));
 
 export const routes: RouteObject[] = [
+	// DEV: Change default path to Home
 	{
 		path: "/",
-		element: <Home />,
+		element: <TestHome />,
 	},
 	{
 		path: "/home",
 		element: <Home />,
+	},
+	{
+		path: "/dev",
+		element: <TestHome />,
 	},
 	{
 		path: "*",
