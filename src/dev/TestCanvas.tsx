b424562cc091c9777503eb84e2df3dd0b997fef7
DanielKhojaste <danielkhojaste101@gmail.com>
2026-03-10 16:22:24 -0400

    test(dnd): make canvas a droppable zone

--- src/dev/TestCanvas.tsx
+++ src/dev/TestCanvas.tsx
@@ -1,12 +1,20 @@
-import { DragDropProvider } from "@dnd-kit/react";
+import { DragDropProvider, useDroppable } from "@dnd-kit/react";
 import TestPlayer from "./TestPlayer";
 import { useRef } from "react";
 
 function TestCanvas() {
 	const canvasRef = useRef<HTMLElement | null>(null);
+	const { ref: droppableRef } = useDroppable({
+		id: "test-canvas",
+	});
+
+	const combinedRef = (element: HTMLElement) => {
+		canvasRef.current = element;
+		droppableRef(element);
+	};
 
 	return (
-		<section ref={canvasRef} id="canvas" className="dev-border">
+		<section ref={combinedRef} id="canvas" className="dev-border">
 			<TestPlayer canvasRef={canvasRef} name="Aitana" jerseyNumber={14} />
 		</section>
 	);
