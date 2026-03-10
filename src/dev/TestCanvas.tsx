20664a179e5d1148530008f72fc98e1d1ed53dc3
DanielKhojaste <danielkhojaste101@gmail.com>
2026-03-10 15:05:03 -0400

    test(dnd): restrict player nodes to the canvas using the canvas ref

--- src/dev/TestCanvas.tsx
+++ src/dev/TestCanvas.tsx
@@ -1,11 +1,14 @@
 import { DragDropProvider } from "@dnd-kit/react";
 import TestPlayer from "./TestPlayer";
+import { useRef } from "react";
 
 function TestCanvas() {
+	const canvasRef = useRef<HTMLElement | null>(null);
+
 	return (
-		<section id="canvas" className="dev-border">
+		<section ref={canvasRef} id="canvas" className="dev-border">
 			<DragDropProvider>
-				<TestPlayer name="Aitana" jerseyNumber={14} />
+				<TestPlayer canvasRef={canvasRef} name="Aitana" jerseyNumber={14} />
 			</DragDropProvider>
 		</section>
 	);
--- src/dev/TestPlayer.tsx
+++ src/dev/TestPlayer.tsx
@@ -1,17 +1,25 @@
+import { RestrictToElement } from "@dnd-kit/dom/modifiers";
 import { useDraggable } from "@dnd-kit/react";
+import { RefObject } from "react";
 
 type TestPlayerProps = {
+	canvasRef: RefObject<HTMLElement | null>;
 	name: string;
 	jerseyNumber: number;
 };
 
-function TestPlayer({ name, jerseyNumber }: TestPlayerProps) {
+function TestPlayer({ canvasRef, name, jerseyNumber }: TestPlayerProps) {
 	const { ref, handleRef } = useDraggable({
 		id: `player-${name}`,
 		data: {
 			from: "canvas-node",
 			type: "test-player",
 		},
+		modifiers: [
+			RestrictToElement.configure({
+				element: () => canvasRef.current,
+			}),
+		],
 	});
 
 	return (
