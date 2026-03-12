964bf9d1db1e2dca195de43cd3ca911e6b2fbd71
DanielKhojaste <danielkhojaste101@gmail.com>
2026-03-12 08:51:54 -0400

    test(dnd): maintain nodes state in home and pass them to canvas

--- src/dev/TestHome.tsx
+++ src/dev/TestHome.tsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
 import { DragDropProvider } from "@dnd-kit/react";
 import TestCanvas from "./TestCanvas";
 import TestToolbar from "./TestToolbar";
+import { NodeFactory } from "../models/NodeFactory";
+import { Node } from "../models/Node";
+import { NodeType } from "../models/NodeType";
 
 function TestHome() {
+	const [nodes, setNodes] = useState<Node[]>([
+		NodeFactory.create(NodeType.Player, {
+			x: 0,
+			y: 0,
+			jerseyNumber: 14,
+			name: "Bonmati",
+		}),
+		NodeFactory.create(NodeType.Cone, { x: 100, y: 100 }),
+	]);
+
 	/**
 	 * Using `any` for `event` to keep code simple and readable.
 	 * TypeScript would otherwise require explicit types here.
@@ -11,14 +25,22 @@ function TestHome() {
 	 * Contributors can safely update this if the provider changes types.
 	 */
 	function handleDragStart(event: any) {
+		console.log("---- handleDragStart ----");
+		console.log(event);
 		console.log(event.operation);
+	}
+
+	function handleDragEnd(event: any) {
+		console.log("---- handleDragEnd ----");
+		console.log(event);
+		const { transform } = event.operation;
 	}
 
 	return (
 		<main className="testHome view">
-			<DragDropProvider onDragStart={handleDragStart}>
+			<DragDropProvider onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
 				<TestToolbar />
-				<TestCanvas />
+				<TestCanvas nodes={nodes} />
 			</DragDropProvider>
 		</main>
 	);
