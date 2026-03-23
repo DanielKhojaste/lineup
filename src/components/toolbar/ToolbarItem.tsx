4418396ca4555598ce4822cdedf371f2b1c8616b
DanielKhojaste <danielkhojaste101@gmail.com>
2026-03-23 11:56:55 -0400

    refactor(toolbarItem): use nodePreview in the toolbar node palette

--- src/components/toolbar/ToolbarItem.tsx
+++ src/components/toolbar/ToolbarItem.tsx
@@ -2,6 +2,7 @@ import { useDraggable } from "@dnd-kit/react";
 import { NodeType } from "../../models/NodeType";
 import NodeRenderer from "../nodes/NodeRenderer";
 import { NodeFactory } from "../../models/NodeFactory";
+import NodePreview from "./NodePreview";
 
 function ToolbarItem({ type }: { type: NodeType }) {
 	const { ref, handleRef } = useDraggable({
@@ -14,9 +15,7 @@ function ToolbarItem({ type }: { type: NodeType }) {
 
 	const previewNode = NodeFactory.create(type, { x: 0, y: 0 });
 
-	return (
-		<NodeRenderer node={previewNode} containerRef={ref} handleRef={handleRef} />
-	);
+	return <NodePreview type={type} containerRef={ref} handleRef={handleRef} />;
 }
 
 export default ToolbarItem;
