695afc4c58cac5ee5d072c6bd79bc4c3ec4b16cd
DanielKhojaste <danielkhojaste101@gmail.com>
2026-03-23 12:29:04 -0400

    refactor(node): remove unused moveTo function

--- src/models/Node.ts
+++ src/models/Node.ts
@@ -1,15 +1,4 @@
-import { clamp } from "../utils/helpers";
 import { NodeType } from "./NodeType";
-
-type PositionType = {
-	x: number;
-	y: number;
-};
-
-type BoundsType = {
-	width: number;
-	height: number;
-};
 
 export abstract class Node {
 	constructor(
@@ -26,10 +15,5 @@ export abstract class Node {
 	public moveBy(dx: number, dy: number) {
 		this.x += dx;
 		this.y += dy;
-	}
-
-	public moveTo(position: PositionType, bounds: BoundsType) {
-		this.x = clamp(position.x, 0, bounds.width);
-		this.y = clamp(position.y, 0, bounds.height);
 	}
 }
